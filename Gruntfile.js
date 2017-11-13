11f026cd6586fa6d10a58158137fe3d971e3648e
selu91 <cristea.marius.alexandru@gmail.com>
2017-11-13 12:42:40 +0200

    Added faq builder task. [skip ci]

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,5 +8,6 @@ module.exports = function (grunt) {
     config = config();
     config.files.php.push( '!inc/PhpFormBuilder.php' );
     config.files.php.push( '!mailin.php' );
+	config.taskMap['faq_builder'] = 'grunt-helpscout-faq';
     loader( grunt, config ).init();
 };
