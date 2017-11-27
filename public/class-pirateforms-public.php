d96b18c0550c787b898786029c3c4a5c64e18d12
Bogdan <bogdan@themeisle.com>
2017-11-27 17:08:12 +0200

    Changed order of rendering captcha

--- public/class-pirateforms-public.php
+++ public/class-pirateforms-public.php
@@ -367,7 +367,7 @@ class PirateForms_Public {
 				'class' => 'pirate-forms-submit-button btn btn-primary',
 				'wrap'  => array(
 					'type'  => 'div',
-					'class' => implode( ' ', apply_filters( 'pirateform_wrap_classes_submit', array( 'col-xs-12 form_field_wrap contact_submit_wrap' ) ) ),
+					'class' => implode( ' ', apply_filters( 'pirateform_wrap_classes_submit', array( 'col-xs-12 col-sm-6 form_field_wrap contact_submit_wrap' ) ) ),
 				),
 				'value' => $pirateformsopt_label_submit_btn,
 			);
