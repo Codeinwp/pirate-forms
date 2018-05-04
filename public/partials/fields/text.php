de3b60fcf3ba9c411b002f40d1002999d11a7d87
contactashish13 <contactashish13@gmail.com>
2018-05-05 00:00:12 +0530

    compare with zerif

--- public/partials/fields/text.php
+++ public/partials/fields/text.php
@@ -13,7 +13,7 @@
 <?php
 if ( is_null( $wrap_classes ) ) {
 	$wrap_classes = array(
-		'col-xs-12 col-sm-4',
+		'col-xs-12',
 		'pirate_forms_three_inputs form_field_wrap',
 		"contact_{$name}_wrap",
 		isset( $args['wrap_class'] ) ? $args['wrap_class'] : '',
