ab3602b6c76f39cedc5e4f9fb4ecd53e35542d00
contactashish13 <contactashish13@gmail.com>
2018-05-12 16:28:53 +0530

    exclude gutenberg related js

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,8 @@ module.exports = function (grunt) {
     config = config();
     config.files.php.push( '!inc/PhpFormBuilder.php' );
     config.files.php.push( '!mailin.php' );
+    config.files.js.push( '!gutenberg/**/*.js' );
+    config.files.js.push( '!webpack.config.js' );
 	config.taskMap.faq_builder = 'grunt-helpscout-faq';
     loader( grunt, config ).init();
 };
